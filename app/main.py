0d6168fe71776f6e52c82cdcbea93f531e39e3be
Evandro Souza <evandroferreiras@gmail.com>
2018-09-02 15:55:41 -0300

    Modified some metrics

--- app/main.py
+++ app/main.py
@@ -2,19 +2,15 @@ from prometheus_client import start_http_server, Summary, Counter, Histogram
 import random
 import time
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds',
-                       'Time spent processing request')
 
 REQUEST_TIME_HIST = Histogram('request_latency_seconds', 'Histogram')
 
 
-COUNTER = Counter('count_bigger_half_sec',
+COUNTER = Counter('requests_greater_than_half_sec_total',
                   'number of waits greater than half a second')
 
 
 @REQUEST_TIME_HIST.time()
-@REQUEST_TIME.time()
 def process_request(t):
     """A dummy function that takes some time."""
     time.sleep(t)
